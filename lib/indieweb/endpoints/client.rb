c36fda3b30e392ed5445a7885d8194cbd9daad0d
Jason Garber <jason@sixtwothree.org>
2019-04-25 19:37:44 -0400

    Refactor Client#response method

--- lib/indieweb/endpoints/client.rb
+++ lib/indieweb/endpoints/client.rb
@@ -21,16 +21,11 @@ module IndieWeb
       end
 
       def response
-        @response ||= HTTP.follow.headers(HTTP_HEADERS_OPTS).timeout(
-          connect: 10,
-          read: 10
-        ).get(@url)
-      rescue HTTP::ConnectionError => exception
-        raise ConnectionError, exception
-      rescue HTTP::TimeoutError => exception
-        raise TimeoutError, exception
-      rescue HTTP::Redirector::TooManyRedirectsError => exception
-        raise TooManyRedirectsError, exception
+        @response ||= HTTP.follow.headers(HTTP_HEADERS_OPTS).timeout(connect: 10, read: 10).get(@url)
+      rescue HTTP::ConnectionError,
+             HTTP::TimeoutError,
+             HTTP::Redirector::TooManyRedirectsError => exception
+        raise IndieWeb::Endpoints.const_get(exception.class.name.split('::').last), exception
       end
     end
   end
