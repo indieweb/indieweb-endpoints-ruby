18a0a827685fcb2dbef81c505a1b1854bdcf1629
Jason Garber <jason@sixtwothree.org>
2020-02-10 13:52:44 -0500

    Refactor HttpRequestService.get method

--- lib/indieweb/endpoints/services/http_request_service.rb
+++ lib/indieweb/endpoints/services/http_request_service.rb
@@ -2,38 +2,19 @@ module IndieWeb
   module Endpoints
     module Services
       class HttpRequestService
-        # Defaults derived from Webmention specification examples
-        # https://www.w3.org/TR/webmention/#limits-on-get-requests
-        HTTP_CLIENT_OPTS = {
-          follow: {
-            max_hops: 20
-          },
-          headers: {
-            accept: '*/*',
-            user_agent: 'IndieWeb Endpoint Discovery (https://rubygems.org/gems/indieweb-endpoints)'
-          },
-          timeout_options: {
-            connect_timeout: 5,
-            read_timeout: 5
-          }
+        HTTP_HEADERS_OPTS = {
+          accept: '*/*',
+          user_agent: 'IndieWeb Endpoint Discovery (https://rubygems.org/gems/indieweb-endpoints)'
         }.freeze
 
-        def get(uri)
-          client.request(:get, uri)
+        # HTTP options derived from Webmention specification examples
+        # https://www.w3.org/TR/webmention/#limits-on-get-requests
+        def self.get(uri)
+          HTTP.follow(max_hops: 20).headers(HTTP_HEADERS_OPTS).timeout(connect: 5, read: 5).get(uri)
         rescue HTTP::ConnectionError,
                HTTP::TimeoutError,
                HTTP::Redirector::TooManyRedirectsError => exception
           raise IndieWeb::Endpoints.const_get(exception.class.name.split('::').last), exception
-        end
-
-        def self.get(uri)
-          new.get(uri)
-        end
-
-        private
-
-        def client
-          @client ||= HTTP::Client.new(HTTP_CLIENT_OPTS)
         end
       end
     end
